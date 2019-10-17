c88085d3c66d19233ab85fb84acf7e924d47f759
Wade Barnes <wade.barnes@shaw.ca>
2019-10-17 07:35:08 -0700

    Add [openshift-developer-tools](https://github.com/BCDevOps/openshift-developer-tools) compatible settings file.

---
+++ openshift/settings.sh
@@ -0,0 +1,9 @@
+export PROJECT_NAMESPACE="apndkr"
+export GIT_URI="https://github.com/WadeBarnes/jag-shuber-frontend.git"
+export GIT_REF="cass"
+
+# The project components
+export components="nginx-runtime yarn-builder frontend"
+
+# Skip Jenkins pipeline processing for this project.  The pipelines are integrated into the build templates.
+export SKIP_PIPELINE_PROCESSING=1
