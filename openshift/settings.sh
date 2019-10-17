19b6e703eb5f0ad643685f913107c36c56917995
Wade Barnes <wade.barnes@shaw.ca>
2019-10-17 08:55:52 -0700

    Ignore the frontend-deploy-blue-green template

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -7,3 +7,6 @@ export components="nginx-runtime yarn-builder frontend"
 
 # Skip Jenkins pipeline processing for this project.  The pipelines are integrated into the build templates.
 export SKIP_PIPELINE_PROCESSING=1
+
+# The list of templates to ignore
+export ignore_templates="frontend-deploy-blue-green"
